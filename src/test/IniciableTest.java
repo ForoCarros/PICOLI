2a432a54151acee82f3e7e94599201f10496b8cb
PABLO <PABLO@192.168.1.224>
2018-04-24 11:42:07 +0200

    Iniciador y su test

--- src/test/IniciableTest.java
+++ src/test/IniciableTest.java
@@ -4,15 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import acciones.Iniciador;
 import modelo.Datos;
+import utiles.Constantes;
 
 class IniciableTest {
 
 	@Test
 	void test() {
 		Datos datos = new Datos();
-		Iniciador iniciador = new Iniciador(datos);
-		
+		Iniciador instancia = new Iniciador(datos);
+		instancia.sortearColor();
+		assertEquals(Constantes.TAMANO_COLA, datos.getCola().getCollectionCola().size());
+		assertNotNull(datos.getCola().obtener());
 	}
-
 }
