93ee8155664de4c11ad13cf9873bbce8fbe1f1bd
Fernan <36297944+FerRodSan@users.noreply.github.com>
2018-04-24 10:40:34 +0200

    barajar, cambios en pila y juego

--- src/logica/Juego.java
+++ src/logica/Juego.java
@@ -2,9 +2,39 @@ package logica;
 
 import modelo.Datos;
 
-public class Juego {
+public class Juego implements Jugable{
 	
 	Datos datos;
+
+	@Override
+	public void disminuirIntentos(int c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean comprobarGanador() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void incrementarMonedas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean comprobarListaLlena() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Datos getDatos() {
+		return datos;
+	}
+	
+	
 	
 	
 
