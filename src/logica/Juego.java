1ed07060aa678b3d8341c38847999de13e2e1831
Jose <HellsockPS3@gmail.com>
2018-04-24 11:14:40 +0200

    Juego 1.0 String "xDxDxD" = getLista.getLista

--- src/logica/Juego.java
+++ src/logica/Juego.java
@@ -1,10 +1,43 @@
 package logica;
 
-import modelo.Datos;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Juego {
+import modelo.Datos;
+import utiles.Constantes;
+
+public class Juego<T> implements Jugable<T>{
 	
 	Datos datos;
+	Constantes constantes;
+	int monedas=0;
+	
+	@Override
+	public void disminuirIntentos(int c) {
+		c--;
+	}
+
+	@Override
+	public boolean comprobarGanador() {
+		if (monedas>=constantes.CANTIDAD_MAX_MONEDAS) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void incrementarMonedas() {
+		monedas++;
+	}
+
+	@Override
+	public boolean comprobarListaLlena() {
+		//El primer getLista coge el objeto Lista. El segundo coge el arrayList de Lista.
+		if (datos.getLista().getLista().size()>=constantes.TAMANO_LISTA) {
+			return true;
+		}
+		return false;
+	}
 	
 	
 
