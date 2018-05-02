a15356ed0aa8884befb2c9023cc68ff60e89b7de
pablo <peythecrafter@gmail.com>
2018-05-02 16:51:49 +0200

    CAmbios menores

--- src/vista/UI.java
+++ src/vista/UI.java
@@ -30,7 +30,6 @@ public class UI extends JFrame {
 
 	protected JPanel contentPane;
 	protected JPanel panelAcciones;
-	protected JButton btnSeleccionarColor;
 	protected JButton btnPedirColor;
 	protected JButton btnBarajarPilas;
 	protected JComboBox comboColores;
@@ -46,8 +45,11 @@ public class UI extends JFrame {
 	protected JPanel panelColasDos;
 	protected JPanel panelLista;
 	protected JButton btnBorrarColor;
-	private JTextField txtMensaje;
-	private JTextField txtMonedas;
+	protected JTextField txtMensaje;
+	protected JTextField txtMonedas;
+	protected JLabel lblIntentosPEdir;
+	protected JLabel lblIntentosBarajar;
+	protected JLabel lblIntentosBorrar;
 
 	/**
 	 * Create the frame.
@@ -66,7 +68,7 @@ public class UI extends JFrame {
 		gbl_contentPane.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
 		gbl_contentPane.rowWeights = new double[] { 1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		JLabel lblTitulo = new JLabel("HOLA PABLO");
 		lblTitulo.setBorder(new LineBorder(new Color(0, 0, 0), 5, true));
 		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
@@ -78,7 +80,7 @@ public class UI extends JFrame {
 		gbc_lblTitulo.gridx = 1;
 		gbc_lblTitulo.gridy = 0;
 		contentPane.add(lblTitulo, gbc_lblTitulo);
-		
+
 		JLabel lblMonedas = new JLabel("Monedas :");
 		lblMonedas.setForeground(Color.WHITE);
 		lblMonedas.setFont(new Font("Tahoma", Font.BOLD, 15));
@@ -89,7 +91,7 @@ public class UI extends JFrame {
 		gbc_lblMonedas.gridx = 1;
 		gbc_lblMonedas.gridy = 1;
 		contentPane.add(lblMonedas, gbc_lblMonedas);
-		
+
 		txtMonedas = new JTextField();
 		txtMonedas.setOpaque(false);
 		txtMonedas.setAutoscrolls(false);
@@ -115,54 +117,74 @@ public class UI extends JFrame {
 		gbc_panelAcciones.gridy = 2;
 		contentPane.add(panelAcciones, gbc_panelAcciones);
 		GridBagLayout gbl_panelAcciones = new GridBagLayout();
-		gbl_panelAcciones.columnWidths = new int[] { 0, 0 };
-		gbl_panelAcciones.rowHeights = new int[] { 30, 0, 0, 0, 0, 0, 0 };
-		gbl_panelAcciones.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-		gbl_panelAcciones.rowWeights = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+		gbl_panelAcciones.columnWidths = new int[] { 0, 20, 0 };
+		gbl_panelAcciones.rowHeights = new int[] { 30, 0, 0, 0, 0, 0 };
+		gbl_panelAcciones.columnWeights = new double[] { 1.0, 1.0, Double.MIN_VALUE };
+		gbl_panelAcciones.rowWeights = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
 		panelAcciones.setLayout(gbl_panelAcciones);
-		
-				comboColores = new JComboBox(Colores.values());
-				GridBagConstraints gbc_comboColoressdf = new GridBagConstraints();
-				gbc_comboColoressdf.insets = new Insets(0, 0, 5, 0);
-				gbc_comboColoressdf.fill = GridBagConstraints.BOTH;
-				gbc_comboColoressdf.gridx = 0;
-				gbc_comboColoressdf.gridy = 1;
-				panelAcciones.add(comboColores, gbc_comboColoressdf);
 
-		btnSeleccionarColor = new JButton("Seleccionar color");
-		btnSeleccionarColor.setOpaque(false);
-		GridBagConstraints gbc_btnSeleccionarColor = new GridBagConstraints();
-		gbc_btnSeleccionarColor.fill = GridBagConstraints.BOTH;
-		gbc_btnSeleccionarColor.insets = new Insets(0, 0, 5, 0);
-		gbc_btnSeleccionarColor.gridx = 0;
-		gbc_btnSeleccionarColor.gridy = 2;
-		panelAcciones.add(btnSeleccionarColor, gbc_btnSeleccionarColor);
+		comboColores = new JComboBox(Colores.values());
+		GridBagConstraints gbc_comboColoressdf = new GridBagConstraints();
+		gbc_comboColoressdf.insets = new Insets(0, 0, 5, 5);
+		gbc_comboColoressdf.fill = GridBagConstraints.BOTH;
+		gbc_comboColoressdf.gridx = 0;
+		gbc_comboColoressdf.gridy = 1;
+		panelAcciones.add(comboColores, gbc_comboColoressdf);
 
 		btnPedirColor = new JButton("Pedir color");
 		btnPedirColor.setOpaque(false);
 		GridBagConstraints gbc_btnPedirColor = new GridBagConstraints();
 		gbc_btnPedirColor.fill = GridBagConstraints.BOTH;
-		gbc_btnPedirColor.insets = new Insets(0, 0, 5, 0);
+		gbc_btnPedirColor.insets = new Insets(0, 0, 5, 5);
 		gbc_btnPedirColor.gridx = 0;
-		gbc_btnPedirColor.gridy = 3;
+		gbc_btnPedirColor.gridy = 2;
 		panelAcciones.add(btnPedirColor, gbc_btnPedirColor);
+
+		lblIntentosPEdir = new JLabel("");
+		lblIntentosPEdir.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblIntentosPEdir.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblIntentosPEdir = new GridBagConstraints();
+		gbc_lblIntentosPEdir.fill = GridBagConstraints.VERTICAL;
+		gbc_lblIntentosPEdir.insets = new Insets(0, 0, 5, 0);
+		gbc_lblIntentosPEdir.gridx = 1;
+		gbc_lblIntentosPEdir.gridy = 2;
+		panelAcciones.add(lblIntentosPEdir, gbc_lblIntentosPEdir);
 
 		btnBarajarPilas = new JButton("Barajar pilas");
 		btnBarajarPilas.setOpaque(false);
 		GridBagConstraints gbc_btnBarajarPilas = new GridBagConstraints();
 		gbc_btnBarajarPilas.fill = GridBagConstraints.BOTH;
-		gbc_btnBarajarPilas.insets = new Insets(0, 0, 5, 0);
+		gbc_btnBarajarPilas.insets = new Insets(0, 0, 5, 5);
 		gbc_btnBarajarPilas.gridx = 0;
-		gbc_btnBarajarPilas.gridy = 4;
+		gbc_btnBarajarPilas.gridy = 3;
 		panelAcciones.add(btnBarajarPilas, gbc_btnBarajarPilas);
+
+		lblIntentosBarajar = new JLabel("");
+		lblIntentosBarajar.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblIntentosBarajar.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblIntentosBarajar = new GridBagConstraints();
+		gbc_lblIntentosBarajar.insets = new Insets(0, 0, 5, 0);
+		gbc_lblIntentosBarajar.gridx = 1;
+		gbc_lblIntentosBarajar.gridy = 3;
+		panelAcciones.add(lblIntentosBarajar, gbc_lblIntentosBarajar);
 
 		btnBorrarColor = new JButton("Borrar color");
 		GridBagConstraints gbc_btnBorrarColor = new GridBagConstraints();
 		gbc_btnBorrarColor.fill = GridBagConstraints.BOTH;
-		gbc_btnBorrarColor.insets = new Insets(0, 0, 5, 0);
+		gbc_btnBorrarColor.insets = new Insets(0, 0, 5, 5);
 		gbc_btnBorrarColor.gridx = 0;
-		gbc_btnBorrarColor.gridy = 5;
+		gbc_btnBorrarColor.gridy = 4;
 		panelAcciones.add(btnBorrarColor, gbc_btnBorrarColor);
+
+		lblIntentosBorrar = new JLabel("");
+		lblIntentosBorrar.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblIntentosBorrar.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblIntentosBorrar = new GridBagConstraints();
+		gbc_lblIntentosBorrar.fill = GridBagConstraints.VERTICAL;
+		gbc_lblIntentosBorrar.insets = new Insets(0, 0, 5, 0);
+		gbc_lblIntentosBorrar.gridx = 1;
+		gbc_lblIntentosBorrar.gridy = 4;
+		panelAcciones.add(lblIntentosBorrar, gbc_lblIntentosBorrar);
 
 		panelColores = new JPanel();
 		panelColores.setBackground(new Color(128, 128, 128));
@@ -291,7 +313,7 @@ public class UI extends JFrame {
 		gbc_panelLista.gridy = 2;
 		contentPane.add(panelLista, gbc_panelLista);
 		panelLista.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+
 		JLabel lblMensaje = new JLabel("Mensaje :");
 		lblMensaje.setForeground(Color.WHITE);
 		lblMensaje.setHorizontalAlignment(SwingConstants.CENTER);
@@ -302,7 +324,7 @@ public class UI extends JFrame {
 		gbc_lblMensaje.gridx = 1;
 		gbc_lblMensaje.gridy = 3;
 		contentPane.add(lblMensaje, gbc_lblMensaje);
-		
+
 		txtMensaje = new JTextField();
 		txtMensaje.setOpaque(false);
 		txtMensaje.setEnabled(false);
